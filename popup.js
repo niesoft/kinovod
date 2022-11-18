66d97c935c296197243341f26a9ee980cbbf07d6
Niesoft <vdonsk@bk.ru>
2022-11-18 23:31:06 +0300

    Добавил домены на день вперёд и на день назад.

--- popup.js
+++ popup.js
@@ -2,20 +2,39 @@ var kinovod = {
 	updateButton: null,
 	start: function()
 	{
-		this.getDate();
+		this.setTitle();
+		this.setButtons();
 	},
-	getDate: function()
+	getDomain: function(index)
 	{
 		let today = new	Date();
+		if (index > 0) {
+			today = new Date(today.getTime() + (24 * 60 * 60 * 1000));
+		}
+		if (index < 0) {
+			today = new Date(today.getTime() - (24 * 60 * 60 * 1000));
+		}
 		var dd = String(today.getDate()).padStart(2, '0');
 		var mm = String(today.getMonth() + 1).padStart(2, '0');
 		var yy = today.getFullYear();
-		let domain = "kinovod" +  dd + mm + yy.toString().substring(2) + ".cc";
+		return "kinovod" +  dd + mm + yy.toString().substring(2) + ".cc";
+	},
+	setTitle: function()
+	{
+		let domain = this.getDomain(0);
 		let href = document.querySelector(".kinovod .title a");
 		if (href) {
 			href.setAttribute("href", "http://" + domain);
 			href.innerHTML = domain;
 		}
+	},
+	setButtons: function()
+	{
+		let leftButton = document.querySelector(".footer .left");
+		let rightButton = document.querySelector(".footer .right");
+
+		leftButton.setAttribute("href", "http://" + this.getDomain(-1));
+		rightButton.setAttribute("href", "http://" + this.getDomain(1));
 	}
 };
 
